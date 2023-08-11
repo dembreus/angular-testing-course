28b69e8ca69239d8e9dac19796d092a7b3b921e0
Dillon Embreus <35782813+dembreus@users.noreply.github.com>
2023-08-10 20:44:38 -0600

    test: testing services

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,40 @@
+import { CoursesService } from "./courses.service";
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { COURSES } from "../../../../server/db-data";
+
+describe("CoursesService", () => {
+  let coursesService: CoursesService,
+    httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CoursesService],
+    });
+
+    coursesService = TestBed.inject(CoursesService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it("should retrieve all courses", () => {
+    coursesService.findAllCourses().subscribe((courses) => {
+      expect(courses).toBeTruthy("noc courses returned");
+
+      expect(courses.length).toBe(12, "incorrect number of courses");
+
+      const course = courses.find((course) => course.id == 12);
+
+      expect(course.titles.description).toBe("Angular Testing Course");
+    });
+
+    const request = httpTestingController.expectOne("/api/courses");
+
+    expect(request.request.method).toEqual("GET");
+
+    request.flush({ payload: Object.values(COURSES) });
+  });
+});
